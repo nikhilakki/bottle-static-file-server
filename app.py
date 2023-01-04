66a68b7f273e0a0f5a715685654946c34020519c
Nikhil Akki <17634231+nikhilakki@users.noreply.github.com>
2023-01-04 10:39:40 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from pathlib import os
+from bottle import Bottle, run, template, static_file
+
+PATH = os.path.join(os.getcwd(), "files")
+os.makedirs(PATH, exist_ok=True)
+app = Bottle()
+
+
+@app.route("/files/<filename:path>")
+def serve_static(filename):
+    return static_file(filename, root=PATH)
+
+
+@app.route("/")
+def hello():
+    files = os.listdir(PATH)
+    return template(
+        """
+        <h2>Files in folder {{ PATH }}</h2>
+        <p>
+            <ol>
+                % for file in files:
+                <li> <a href=/files/{{ file }}>{{ file }}</a></li>
+                % end
+            </ol>
+        </p>
+    """,
+        PATH=PATH,
+        files=files,
+    )
+
+
+run(app, host="localhost", port=8080)
